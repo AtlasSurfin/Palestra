e400a8aa7ac47ce9a4cdf59ecdaa5fe8fcfcb130
fede <fede@DESKTOP-2FSU8MJ.localdomain>
2026-01-14 00:22:08 +0100

    Updated manager.c

--- manager.c
+++ manager.c
@@ -21,28 +21,6 @@ StatoPalestra *palestra = NULL;
 Config conf;
 char causa_chiusura[20] = "TIMEOUT";
 
-void save_stats(StatoPalestra *p, StatServizio *t, Config c){
-    FILE *f = fopen("stats_simulazione.csv", "a");
-    if(f == NULL){
-        perror("[MANAGER] Errore apertura file CSV");
-        return;
-    }
-    //Se il file è vuoto scrivo l'header
-    fseek(f, 0, SEEK_END);
-    if(ftell(f) == 0){
-        fprintf(f, "Giorno;Atleti_Serviti;Servizi_Persi;Attesa_Media;Rapporto_Istr_Post\n");
-    }
-
-    //Calcolo attesa media
-    double media = (t->serviti_tot > 0) ? (double)t->tempo_attesa_tot / t->serviti_tot : 0;
-    double rapporto = (double)c.nof_workers / c.nof_worker_seats;
-
-    //Scrivo i dati
-    fprintf(f, "%d;%d;%d;%.2f;%.2f\n", p->giorno_corrente, t->serviti_tot, t->non_serviti_tot, media, rapporto);
-    fclose(f);
-    printf("[MANAGER] Statistiche salvate in stats_simulazione.csv\n");
-}
-
 //gestore segnale per SIGINT
 
 void handle_int(int sig){
@@ -58,8 +36,134 @@ void handle_tick(int sig){
     if(palestra) palestra->min_correnti = min_trascorsi;
 }
 
+int main(){
+    //Leggo da file di configurazione
+    conf = load_conf("palestra.conf");
+    pid_manager = getpid();
+
+    struct sigaction sa;
+
+    //Configurazione per SIGUSR1
+    sa.sa_handler = handle_tick;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    sigaction(SIGUSR1, &sa, NULL);
+
+    //Configurazione per SIGINT
+    sa.sa_handler = handle_int;
+    sa.sa_flags = 0;
+    sigaction(SIGINT, &sa, NULL);
 
 
+    //Creazione risorse IPC
+    shmid = shmget(IPC_PRIVATE, sizeof(StatoPalestra), IPC_CREAT | 0666);
+    palestra = (StatoPalestra *)shmat(shmid, NULL, 0);
+
+    //Inizializzazione memoria
+    memset(palestra, 0, sizeof(StatoPalestra));
+    msgid = msgget(IPC_PRIVATE, IPC_CREAT | 0666);
+
+
+    //Inizializzazione semaforo per stats
+    sem_init(&(palestra->mux_stats), 1, 1);
+    palestra->min_correnti = 0;
+    palestra->giorno_corrente = 0;
+    palestra->totale_operatori_attivi = conf.nof_workers;
+
+    //Lancio erogatore
+    if((pid_erogatore = fork()) == 0) lancia_processo("./erogatore", 0, shmid, msgid);
+
+    atleti_pids = malloc(sizeof(pid_t) * conf.nof_users);
+    istruttori_pids = malloc(sizeof(pid_t) * conf.nof_workers);
+
+    //Lancio Istruttori
+    for(int i = 0; i < conf.nof_workers; i++){
+        if((istruttori_pids[i] = fork()) == 0) lancia_processo("./istruttore", i, shmid, msgid);
+    }
+
+    //Lancio Atleti
+    for(int i = 0; i < conf.nof_users; i++){
+        if((atleti_pids[i] = fork()) == 0) lancia_processo("./atleta", i, shmid, msgid);
+    }
+
+    //Barriera di Inizializzazione
+    printf("[MANAGER] Attesa inizializzazione processi figli...\n");
+    sleep(1);
+
+    //Cronometro
+    if((pid_cronometro = fork()) == 0){
+        while(1){
+            sleep_min(1, conf.n_nano_secs); //Conta il tempo simulato: 1 minuto
+            kill(getppid(), SIGUSR1); //invia il segnale al MANAGER
+        }
+    }
+
+    //Logica a giornate
+    int g;
+    for(g = 0; g < conf.sim_duration; g++){
+        palestra->giorno_corrente = g;
+        palestra->min_correnti = 0;
+        min_trascorsi = 0;
+
+        //Assegno servizi e alle postazioni
+        for(int i = 0; i < conf.nof_worker_seats; i++){
+            palestra->servizio_postazione[i] = i % NOF_SERVICES;
+        }
+
+        printf("--- [MANAGER] Inizio Giorno %d ---\n", g + 1);
+
+        //Simuliamo una giornata di 400 minuti (= 8 ore)
+        while(min_trascorsi < 400) pause(); //Aspettiamo il tick del cronometro
+
+        //Pulizia coda per giorno succ. + conteggio reale
+             //Pulizia coda per giorno successivo
+        struct msg_pacco dummy;
+        int msg_residui = 0;
+
+        sem_wait(&(palestra->mux_stats)); //Proteggo accesso massiccio a risorse
+
+        while(1){
+            if(msgrcv(msgid, &dummy, sizeof(struct msg_pacco) - sizeof(long), 0, IPC_NOWAIT) == -1){
+                if(errno == ENOMSG) break;
+                perror("[MANAGER] Errore critico durante svuotamento coda");
+                cleanup();
+                exit(EXIT_FAILURE);
+            }
+
+            int s = dummy.service_type;
+            if(s >= 0 && s < NOF_SERVICES){
+                palestra->stats[s].non_serviti_oggi++;
+                palestra->stats[s].non_serviti_tot++;
+            }
+            msg_residui++;
+        }
+
+        sem_post(&(palestra->mux_stats)); ///Rilascio il semaforo precedente
+
+        print_report(palestra, conf);
+        save_stats(palestra, NULL, conf);
+
+        if(msg_residui > conf.explode_threshold){
+            printf("[MANAGER] SOGLIA CRITICA SUPERATA (%d)! Chiusura.\n", msg_residui);
+            strncpy(causa_chiusura, "EXPLODE", 20);
+            break;
+        }
+
+        //Reset stats per giorno succ.
+
+        sem_wait(&(palestra->mux_stats));
+        for(int i = 0; i < NOF_SERVICES; i++){
+            palestra->stats[i].serviti_oggi = 0;
+            palestra->stats[i].non_serviti_oggi= 0;
+            palestra->stats[i].tempo_attesa_oggi = 0;
+            palestra->stats[i].tempo_erogazione_oggi = 0;
+        }
+        sem_post(&(palestra->mux_stats));
+ }
+
+    cleanup();
+    return 0;
+}
 
 void lancia_processo(char *path, int id, int shmid, int msgid){
     char s_shm[12], s_msg[12], s_id[12];
@@ -70,47 +174,6 @@ void lancia_processo(char *path, int id, int shmid, int msgid){
     execv(args[0], args);
     perror("Execv fallita !");
     exit(EXIT_FAILURE);
-}
-
-void print_report(StatoPalestra *p, Config c){
-    printf("\n======== REPORT GIORNATA %d ========\n", p->giorno_corrente + 1);
-
-    int serviti_oggi = 0;
-
-    printf("%-15s  | %-8s | %-8s | %-12s | %-12s\n", "Servizio", "Serviti", "Persi", "Attesa Med", "Erogaz Med");
-    printf("------------------------------------------------------------\n");
-
-    for(int i = 0; i < NOF_SERVICES; i++){
-        serviti_oggi += p->stats[i].serviti_oggi; 
-        double attesa_media = (p->stats[i].serviti_oggi > 0) ? (double)p->stats[i].tempo_attesa_oggi / p->stats[i].serviti_oggi : 0;
-        double ero_med = (p->stats[i].serviti_oggi > 0) ? (double)p->stats[i].tempo_erogazione_oggi / p->stats[i].serviti_oggi : 0;
-
-        printf("Servizio %-6d | %-8d | %-8d | %-10.2f min | %-10.2f min\n", i, p->stats[i].serviti_oggi, p->stats[i].non_serviti_oggi, attesa_media, ero_med);
-    }
-
-    printf("------------------------------------------------------\n");
-    printf("Utenti serviti oggi: %d | Rapporto Istruttori/Postazioni: %.2f\n", serviti_oggi, (double)p->totale_operatori_attivi / c.nof_worker_seats);
-    printf("==========================================================\n\n");
-
-    }
-
-void print_report_tot(StatoPalestra *p, int giorni_effettivi){
-    printf("\n********** REPORT FINALE **********\n");
-    int tot_serviti = 0, tot_persi = 0;
-    long tot_attesa = 0;
-
-    for(int i = 0; i < NOF_SERVICES; i++){
-        tot_serviti += p->stats[i].serviti_tot;
-        tot_persi += p->stats[i].non_serviti_tot;
-        tot_attesa += p->stats[i].tempo_attesa_tot;
-    }
-
-    printf("Giorni simulati: %d\n", giorni_effettivi);
-    printf("Totale utenti serviti: %d (Media/giorno: %.2f)\n", tot_serviti, (double)tot_serviti / giorni_effettivi);
-    printf("Totale servizi persi: %d\n", tot_persi);
-    printf("Tempo medio attesa globale: %.2f min\n", (tot_serviti > 0) ? (double)tot_attesa / tot_serviti : 0);
-    printf("Pause totali: %d\n", p->pause_tot);
-    printf("****************************************************\n");
 }
 
 void cleanup(){
@@ -166,170 +229,72 @@ void cleanup(){
 
 }
 
-int main(){
-    //Leggo da file di configurazione
-    conf = load_conf("palestra.conf");
-    pid_manager = getpid();
+void save_stats(StatoPalestra *p, StatServizio *t, Config c){
+    FILE *f = fopen("stats_simulazione.csv", "a");
+    if(f == NULL) return;
 
-    struct sigaction sa;
-
-    //Configurazione per SIGUSR1
-    sa.sa_handler = handle_tick;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
-    sigaction(SIGUSR1, &sa, NULL);
-
-    //Configurazione per SIGINT
-    sa.sa_handler = handle_int;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    sigaction(SIGINT, &sa, NULL);
-
-
-    //Creazione risorse IPC
-    shmid = shmget(IPC_PRIVATE, sizeof(StatoPalestra), IPC_CREAT | 0666);
-    palestra = (StatoPalestra *)shmat(shmid, NULL, 0);
-
-    //Inizializzazione memoria
-    memset(palestra, 0, sizeof(StatoPalestra));
-    msgid = msgget(IPC_PRIVATE, IPC_CREAT | 0666);
-
-
-    //Inizializzazione semaforo per stats
-    sem_init(&(palestra->mux_stats), 1, 1);
-    palestra->min_correnti = 0;
-    palestra->giorno_corrente = 0;
-    palestra->totale_operatori_attivi = conf.nof_workers;
-
-    //Lancio erogatore
-    if((pid_erogatore = fork()) == 0) lancia_processo("./erogatore", 0, shmid, msgid);
-
-    atleti_pids = malloc(sizeof(pid_t) * conf.nof_users);
-    istruttori_pids = malloc(sizeof(pid_t) * conf.nof_workers);
-
-    //Lancio Istruttori
-    for(int i = 0; i < conf.nof_workers; i++){
-        if((istruttori_pids[i] = fork()) == 0) lancia_processo("./istruttore", i, shmid, msgid);
+    //Se il file è vuoto scrivo l'header
+    fseek(f, 0, SEEK_END);
+    if(ftell(f) == 0){
+        fprintf(f, "Giorno;Atleti_Serviti;Servizi_Persi;Attesa_Media;Rapporto_Istr_Post\n");
     }
 
-    //Lancio Atleti
-    for(int i = 0; i < conf.nof_users; i++){
-        if((atleti_pids[i] = fork()) == 0) lancia_processo("./atleta", i, shmid, msgid);
-    }
-
-    //Barriera di Inizializzazione
-    printf("[MANAGER] Attesa inizializzazione processi figli...\n");
-    sleep(1);
-
-    //Cronometro
-    if((pid_cronometro = fork()) == 0){
-        while(1){
-            sleep_min(1, conf.n_nano_secs); //Conta il tempo simulato: 1 minuto
-            kill(getppid(), SIGUSR1); //invia il segnale al MANAGER
-        }
-    }
-
-    //Logica a giornate
-    int g;
-    for(g = 0; g < conf.sim_duration; g++){
-        palestra->giorno_corrente = g;
-        min_trascorsi = 0;
-
-        //Assegno servizi e alle postazioni
-        for(int i = 0; i < conf.nof_worker_seats; i++){
-            palestra->servizio_postazione[i] = i % NOF_SERVICES;
-        }
-
-        printf("--- [MANAGER] Inizio Giorno %d ---\n", g + 1);
-
-        //Simuliamo una giornata di 400 minuti (= 8 ore)
-        while(min_trascorsi < 400){
-            pause(); //Aspettiamo il tick del cronometro
-        }
-
-        //Calcolo atleti persi
-        struct msqid_ds q_status;
-        msgctl(msgid, IPC_STAT, &q_status); //Legge stato da coda kernel
-        int persi_oggi = q_status.msg_qnum;
-
-        //Pulizia coda per giorno succ. + conteggio reale
-             //Pulizia coda per giorno successivo
-        struct msg_pacco dummy;
-        int msg_residui = 0;
-
-        sem_wait(&(palestra->mux_stats)); //Proteggo accesso massiccio a risorse
-
-        while(1){
-            if(msgrcv(msgid, &dummy, sizeof(struct msg_pacco) - sizeof(long), 0, IPC_NOWAIT) == -1){
-                if(errno == ENOMSG) break;
-                perror("[MANAGER] Errore critico durante svuotamento coda");
-                cleanup();
-                exit(EXIT_FAILURE);
-            }
-
-            int s = dummy.service_type;
-            if(s >= 0 && s < NOF_SERVICES){
-                palestra->stats[s].non_serviti_oggi++;
-                palestra->stats[s].non_serviti_tot++;
-            }
-            msg_residui++;
-        }
-        if(msg_residui > 0){
-            printf("[MANAGER] Giornata finita: rimossi %d messaggi totali.\n", msg_residui);
-        }
-
-        sem_post(&(palestra->mux_stats)); ///Rilascio il semaforo precedente
-
-        if(msg_residui > 0){
-            printf("[MANAGER] Giornata finita: rimossi %d messaggi.\n", msg_residui);
-        }
-
-        print_report(palestra, conf);
-
-        if(msg_residui > conf.explode_threshold){
-            strcpy(causa_chiusura, "EXPLODE");
-            g++;
-            break;
-        }
-
-        //Reset stats per giorno succ.
-
-        sem_wait(&(palestra->mux_stats));
+    StatServizio locale ={0};
+    if(t == NULL){
         for(int i = 0; i < NOF_SERVICES; i++){
-            palestra->stats[i].serviti_oggi = 0;
-            palestra->stats[i].non_serviti_oggi= 0;
-            palestra->stats[i].tempo_attesa_oggi = 0;
-            palestra->stats[i].tempo_erogazione_oggi = 0;
+            locale.serviti_tot += p->stats[i].serviti_oggi;
+            locale.non_serviti_tot += p->stats[i].non_serviti_oggi;
+            locale.tempo_attesa_tot += p->stats[i].tempo_attesa_oggi;
         }
-        sem_post(&(palestra->mux_stats));
-        
+        t = &locale;
+    }
+    //Calcolo attesa media
+    double media = (t->serviti_tot > 0) ? (double)t->tempo_attesa_tot / t->serviti_tot : 0;
+    double rapporto = (double)c.nof_workers / c.nof_worker_seats;
 
-        //Stampa statistiche e controllo Explode
-        print_report(palestra, conf);
+    //Scrivo i dati
+    fprintf(f, "%d;%d;%d;%.2f;%.2f\n", p->giorno_corrente + 1, t->serviti_tot, t->non_serviti_tot, media, rapporto);
+    fclose(f);
+    printf("[MANAGER] Statistiche salvate in stats_simulazione.csv\n");
+}
 
-        if(persi_oggi > conf.explode_threshold){
-            strcpy(causa_chiusura, "EXPLODE");
-            g++;
-            break;
-        }
+void print_report(StatoPalestra *p, Config c){
+    printf("\n======== REPORT GIORNATA %d ========\n", p->giorno_corrente + 1);
 
+    int serviti_oggi = 0;
 
-   
-        
-        
-        
+    printf("%-15s  | %-8s | %-8s | %-12s | %-12s\n", "Servizio", "Serviti", "Persi", "Attesa Med", "Erogaz Med");
+    printf("------------------------------------------------------------\n");
 
-        //Reset stats per giorno successivo
-        sem_wait(&(palestra->mux_stats));
-        for(int i = 0; i < NOF_SERVICES; i++){
-            palestra->stats[i].serviti_oggi = 0;
-            palestra->stats[i].non_serviti_oggi = 0;
-            palestra->stats[i].tempo_attesa_oggi = 0;
-            palestra->stats[i].tempo_erogazione_oggi = 0;
-        }
-        sem_post(&(palestra->mux_stats));
+    for(int i = 0; i < NOF_SERVICES; i++){
+        serviti_oggi += p->stats[i].serviti_oggi; 
+        double attesa_media = (p->stats[i].serviti_oggi > 0) ? (double)p->stats[i].tempo_attesa_oggi / p->stats[i].serviti_oggi : 0;
+        double ero_med = (p->stats[i].serviti_oggi > 0) ? (double)p->stats[i].tempo_erogazione_oggi / p->stats[i].serviti_oggi : 0;
+
+        printf("Servizio %-6d | %-8d | %-8d | %-10.2f min | %-10.2f min\n", i, p->stats[i].serviti_oggi, p->stats[i].non_serviti_oggi, attesa_media, ero_med);
     }
 
-    cleanup();
-    return 0;
+    printf("------------------------------------------------------\n");
+    printf("Utenti serviti oggi: %d | Rapporto Istruttori/Postazioni: %.2f\n", serviti_oggi, (double)p->totale_operatori_attivi / c.nof_worker_seats);
+    printf("==========================================================\n\n");
+
+    }
+
+void print_report_tot(StatoPalestra *p, int giorni_effettivi){
+    printf("\n********** REPORT FINALE **********\n");
+    int tot_serviti = 0, tot_persi = 0;
+    long tot_attesa = 0;
+
+    for(int i = 0; i < NOF_SERVICES; i++){
+        tot_serviti += p->stats[i].serviti_tot;
+        tot_persi += p->stats[i].non_serviti_tot;
+        tot_attesa += p->stats[i].tempo_attesa_tot;
+    }
+
+    printf("Giorni simulati: %d\n", giorni_effettivi);
+    printf("Totale utenti serviti: %d (Media/giorno: %.2f)\n", tot_serviti, (double)tot_serviti / giorni_effettivi);
+    printf("Totale servizi persi: %d\n", tot_persi);
+    printf("Tempo medio attesa globale: %.2f min\n", (tot_serviti > 0) ? (double)tot_attesa / tot_serviti : 0);
+    printf("Pause totali: %d\n", p->pause_tot);
+    printf("****************************************************\n");
 }
