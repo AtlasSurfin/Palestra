822f573c7c667e9f4dca748ca385e510824c4cb7
fede <fede@DESKTOP-2FSU8MJ.localdomain>
2026-01-15 19:36:30 +0100

    Project v1.2 started: uploaded atleta.c, editing istruttore.c

---
+++ v1.2/config.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "config.h"
+
+Config load_conf(const char *filename){
+    Config conf = {0}; //per sicurezza, inizializzo tutto a zero
+    FILE *file = fopen(filename, "r");
+
+    if(!file){
+        perror("Errore critico: Impossibile aprire il file .conf");
+        exit(EXIT_FAILURE);
+    }
+
+    char linea[256];
+    while(fgets(linea, sizeof(linea), file)){
+        //Salta commenti e righe vuote
+        if(linea[0] == '\n' || linea[0] == '\r' || linea[0] == '#') continue;
+
+        char chiave[64];
+        double valore_dec;
+        //legge tutto fino ad uguale, poi cerca il numero dopo l'uguale
+        if(sscanf(linea, "%[^=]=%lf", chiave, &valore_dec) == 2){
+            strtok(chiave, " \t\n\r");
+            if(strcmp(chiave, "SIM_DURATION") == 0) conf.sim_duration = (long)valore_dec;
+            else if(strcmp(chiave, "N_NANO_SECS") == 0) conf.n_nano_secs = (long)valore_dec;
+            else if(strcmp(chiave, "NOF_WORKERS") == 0) conf.nof_workers = (int)valore_dec;
+            else if(strcmp(chiave, "NOF_USERS") == 0) conf.nof_users = (int)valore_dec;
+            else if(strcmp(chiave, "NOF_WORKER_SEATS") == 0) conf.nof_worker_seats = (int)valore_dec;
+            else if(strcmp(chiave, "NOF_PAUSE") == 0) conf.nof_pause = (int)valore_dec;
+            else if(strcmp(chiave, "EXPLODE_THRESHOLD") == 0) conf.explode_threshold = (int)valore_dec;
+            else if(strcmp(chiave, "P_SERV_MIN") == 0) conf.p_serv_min = (double)valore_dec;
+            else if(strcmp(chiave, "P_SERV_MAX") == 0) conf.p_serv_max = (double)valore_dec;
+            else if(strcmp(chiave, "N_REQUESTS") == 0) conf.n_requests = (int)valore_dec;
+        }
+    }
+
+    fclose(file);
+    return conf;
+}
---
+++ v1.2/config.h
@@ -0,0 +1,19 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+typedef struct{
+    long sim_duration;
+    long n_nano_secs;
+    int nof_workers;
+    int nof_worker_seats;
+    int nof_users;
+    int nof_pause;
+    int explode_threshold;
+    double p_serv_min;
+    double p_serv_max;
+    int n_requests;
+}Config;
+
+Config load_conf(const char *filename);
+
+#endif
