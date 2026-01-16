eaa59cfe462e7f9ee7fcb95d3a65fdc66a205fc1
fede <fede@DESKTOP-2FSU8MJ.localdomain>
2026-01-16 19:43:43 +0100

    Makefile v1.1 updated. Files in v1.2 updated

---
+++ v1.1/atleta.c
@@ -0,0 +1,119 @@
+#include "common.h"
+#include "config.h"
+#include <signal.h>
+
+StatoPalestra *palestra = NULL;
+
+
+void handle_term(int sig){
+    (void)sig;
+    if(palestra != (void *)-1 && palestra != NULL) shmdt(palestra);
+    _exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char *argv[]){
+
+    //Configurazione segnali
+    struct sigaction sa;
+    sa.sa_handler = handle_term;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGTERM, &sa, NULL);
+
+
+    //COntrollo args: 0 = nome, 1 = shmid, 2 = msgid, 3 = id_atleta
+    if(argc < 5){
+        fprintf(stderr, "[ATLETA] Errore: args insufficienti in %s\n", argv[0]);
+        exit(EXIT_FAILURE);}
+
+    //Carico la config
+    char *nome_conf = argv[4];
+    Config conf = load_conf(nome_conf);
+
+    //Recupero ID passati dal manager come stringhe
+    int shmid = atoi(argv[1]);
+    int msgid = atoi(argv[2]);
+    int id_atleta = atoi(argv[3]);
+
+    //Risorse IPC
+    palestra = (StatoPalestra *)shmat(shmid, NULL, 0);
+    if(palestra == (void *)-1){
+        perror("[ATLETA] Errore shmat");
+        exit(EXIT_FAILURE);
+    }
+
+    int semid = semget(SEM_KEY, 2, 0666);
+    if(semid == -1){
+        perror("[ATLETA] Errore semget");
+        exit(EXIT_FAILURE);
+    }
+
+    barrier_signal(semid);
+
+    //Seed per rand unico per ogni processo
+    srand(time(NULL) ^ (getpid() << 16));
+
+    
+    //Probabilità di andare in palestra
+    //printf("[ATLETA %d] Caricato: P_MIN=%.2f, P_MAX=%.2f\n", id_atleta, conf.p_serv_min, conf.p_serv_max);//Debug
+
+    double p_serv = conf.p_serv_min + ((double)rand() / RAND_MAX) * (conf.p_serv_max - conf.p_serv_min);
+    int ultimo_giorno_gestito = -1;
+
+    while(1){
+        //Aspetto inizio nuovo giorno
+        while(palestra->giorno_corrente <= ultimo_giorno_gestito) usleep(50000);//100ms di attesa
+
+            ultimo_giorno_gestito = palestra->giorno_corrente;
+            double daily_thres = (double)rand() / RAND_MAX;
+
+            //Decido se venire in palestra oggi
+            if(daily_thres < p_serv){
+                int servizio = rand() % NOF_SERVICES;
+                struct msg_pacco msg;
+
+                //Richiesta ticket all'erogatore
+                msg.tkt_num = 0;
+                msg.mtype = 1; //Richieste per erogatore
+                msg.sender_id = getpid();
+                msg.service_type = servizio;
+                msg.min_inizio_attesa = palestra->min_correnti;
+
+                if(msgsnd(msgid, &msg, sizeof(struct msg_pacco) - sizeof(long), IPC_NOWAIT) == -1){
+                    if(errno == EAGAIN){
+                        printf("[ATLETA %d] Coda erogatore piena. Torno a casa...\n", id_atleta);
+                        goto fine_giornata;
+                    }
+                    break;
+                }
+
+                //Ricezione ticket
+                if(msgrcv(msgid, &msg, sizeof(struct msg_pacco) - sizeof(long), getpid(), IPC_NOWAIT) == -1){
+                    if(errno == EINVAL || errno == EIDRM) break;
+                }
+
+                printf("[ATLETA %d] Giorno %d: Vado ! Preso il ticket %d per servizio %d\n", 
+                        id_atleta, ultimo_giorno_gestito + 1, msg.tkt_num, servizio);
+
+                //Entrata in coda servizio
+                msg.mtype = 10 + servizio; //mtype speciale 
+                msg.sender_id = id_atleta;
+                if(msgsnd(msgid, &msg, sizeof(struct msg_pacco) - sizeof(long), 0) == -1){
+                    if(errno == EAGAIN){
+                        printf("[ATLETA %d] Coda servizio %d piena. Ci rinuncio, torno a casa...\n", id_atleta, servizio);
+                        goto fine_giornata;
+                    }
+                    break;
+                }
+            }else{
+                printf("[ATLETA %d] Giorno %d: Oggi resto a casa\n", id_atleta, ultimo_giorno_gestito + 1);
+            }                
+            
+fine_giornata:
+            while(palestra->min_correnti < 400 && palestra->giorno_corrente == ultimo_giorno_gestito){
+                usleep(200000);
+            }
+            //Fine allenamento
+        }
+        return 0;
+}
---
+++ v1.1/common.h
@@ -0,0 +1,110 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/ipc.h>
+#include <sys/wait.h>
+#include <sys/msg.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+
+
+#define TESTO_MAX 64
+#define NOF_SERVICES 6
+
+//Costanti per servizi
+#define SERVIZIO_PESI 0
+#define SERVIZIO_CARDIO 1
+#define SERVIZIO_VALUTAZIONE 2
+#define SERVIZIO_CORSO 3
+#define SERVIZIO_NUTRIZIONE 4
+#define SERVIZIO_SHOP 5
+
+//Chiave univoca per set semafori
+#define SEM_KEY 123456
+
+//Indici semafori nel set
+#define MUX_STATS 0
+#define BARRIER_SEM 1
+
+//Strutture per Statistiche
+typedef struct{
+    int serviti_oggi;
+    int serviti_tot;
+    int non_serviti_oggi;
+    int non_serviti_tot;
+    long tempo_attesa_oggi;
+    long tempo_attesa_tot;
+    long tempo_erogazione_oggi;
+    long tempo_erogazione_tot;
+}StatServizio;
+
+
+//Struttura in Memoria Condivisa
+typedef struct{
+    int min_correnti;
+    int giorno_corrente;
+    int pause_tot;
+    int totale_operatori_attivi;
+    int servizio_postazione[20]; //Max 20 postazioni/sportelli
+    StatServizio stats[NOF_SERVICES];
+}StatoPalestra;
+
+//Struttura Messaggi System V
+struct msg_pacco{
+    long mtype;
+    int sender_id;
+    int service_type;
+    int tkt_num;
+    int min_inizio_attesa;
+    char testo[TESTO_MAX];
+};
+
+//Struttura necessaria per semctl
+union semun {
+    int val;
+    struct semid_ds *buf;
+    unsigned short *array;
+};
+
+static inline void sleep_min(int min, long n_nano_secs){
+    struct timespec ts;
+    long long tot_nsecs = (long long) min * n_nano_secs; //forzo calcolo a 64 bit, con long long potrò avere num > 2 miliardi per nsecs
+
+    ts.tv_sec = (time_t)(tot_nsecs / 1000000000);
+    ts.tv_nsec = (long)(tot_nsecs % 1000000000);
+
+    nanosleep(&ts, NULL);
+}
+
+//funzioni per semafori
+static inline void sem_p(int semid, int sem_num){
+    struct sembuf sb = {(unsigned short)sem_num, -1, SEM_UNDO}; //-1 = Wait
+    if(semop(semid, &sb, 1) == -1 && errno != EINTR) perror("Errore sem_p");
+}
+
+static inline void sem_v(int semid, int sem_num){
+    struct sembuf sb = {(unsigned short)sem_num, 1, SEM_UNDO};// 1 = Signal
+     if(semop(semid, &sb, 1) == -1 && errno != EINTR) perror("Errore sem_v");
+}
+
+//funzioni per sem barriera, senza undo
+static inline void barrier_wait(int semid){ 
+    struct sembuf sb = {BARRIER_SEM, -1, 0};
+    if(semop(semid, &sb, 1) == -1 && errno != EINTR) perror("Errore barriera_wait");
+}
+
+static inline void barrier_signal(int semid){
+   struct sembuf sb = {BARRIER_SEM, 1, 0};
+if(semop(semid, &sb, 1) == -1 && errno != EINTR) perror("Errore barriera_signal"); 
+}
+
+#endif
